97f67a7a229e3253f21360a2e8ac77236342fd56
Prayag Desai <prayag2010@live.com>
2020-03-01 20:56:21 -0700

    Added LCD Driver

--- lcd_driver.c
+++ lcd_driver.c
@@ -1,197 +1,239 @@
 /*
- * lcd_driver.c
+ * @file lcd_driver.c
+ * @brief The LCD driver for the BOOSTXL-K350QVG boosterpack
  *
- *  Created on: 03-Dec-2019
- *      Author: prayag
+ * This source file contains the LCD driver for the
+ * BOOSTXL-K350QVG boosterpack. It uses 4-wire SPI to
+ * communicate with the LCD, and is capable og drawing
+ * pixels, lines and text on the display
+ *
+ * PLEASE SEE THE CORRESPONDING HEADER FILES FOR DETAILED
+ * FUNCTION DESCRIPTIONS
+ *
+ * @date 03-Dec-2019
+ * @author Prayag Milan Desai, Rahul Ramaprasad
  */
 
 
 #include "lcd_driver.h"
 
 
-
+//Set the ID[1:0] bits and the AM bit to configure the cursor mode
 void setCursorMode(uint8_t id1, uint8_t id0, uint8_t am)
 {
+    //create the curcor config bits
     uint16_t temp = ((id1 << 2) | (id0 << 1) | am) << 3;
+    //Combine them with the default cursor config
     temp |= 0x6800;
+    //Send the register address and the data created
     lcdSendCommand(SSD2119_ENTRY_MODE_REG);
     spi16bytes(temp);
 }
 
+//Delay for a perticular number of milliseconds
+//Taken from the example LCD driver found at
+//http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP-EXP432P401R/latest/index_FDS.html
 void delay(uint16_t msec)
 {
+    //loop counter
     uint32_t i = 0;
+    //Calculate time taken from clock speed and machine cycles
     uint32_t time = (msec / 1000) * (SYSTEM_CLOCK_SPEED / 15);
 
+    //Run a blank loop
     for(i = 0; i < time; i++)
     {
         ;
     }
 }
 
+//Send the command register address to the LCD
 void lcdSendCommand(uint8_t command)
 {
+    //Select command line
     selectDC(0);
+    //send the command
     spi8bytes(command);
+    //Deselect command line
     selectDC(1);
 }
 
+//Send 16 byes of SPI data
 void spi16bytes(uint16_t in)
 {
-    //    selectCS(1);
+    //Load the MSB
     EUSCI_B0->TXBUF = (uint8_t)(in >> 8);
     while (!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
 
+    //Load the LSB
     EUSCI_B0->TXBUF = (uint8_t)(in & 0xFF);
     while (!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
 
 }
 
+//Send 8 bytes of SPI data
 void spi8bytes(uint8_t in)
 {
-    //    selectCS(1);
+    //load the 8 bytes
     EUSCI_B0->TXBUF = in;
     while (!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
 }
 
+//Select/Deselect the LCD
 void selectCS(int state)
 {
+    //Set or clear the SCS line
     if(state)
         P5->OUT |= LCD_CS;
     else
         P5->OUT &= ~LCD_CS;
 }
 
+//Select/Deselect the LCD command line
 void selectDC(int state)
 {
+    //Set or clear the SDC line
     if(state)
         P4->OUT |= DC;
     else
         P4->OUT &= ~DC;
 }
 
+//Set the position of the cursor in X-Y axis
 void setCursorXY(uint16_t posX, uint16_t posY)
 {
+    //Send the X position
     lcdSendCommand(SSD2119_X_RAM_ADDR_REG);
     spi16bytes(posX);
+    //Send the Y position
     lcdSendCommand(SSD2119_Y_RAM_ADDR_REG);
     spi16bytes(posY);
 }
 
+//Dray a pixel on the LCD
 void drawPixelXY(uint16_t posX, uint16_t posY, uint16_t color)
 {
+    //Select the LCD
     selectCS(0);
+    //Set the cursor
     setCursorXY(posX, posY);
+    //Write the pixel data
     lcdSendCommand(SSD2119_RAM_DATA_REG);
     spi16bytes(color);
+    //Deselect the LCD
     selectCS(1);
 }
 
+//Draw a horizontal line
 void drawLineX(uint16_t startPosX, uint16_t length, uint16_t startPosY, uint16_t color)
 {
+    //Select the LCD
     selectCS(0);
 
+    //Set the X and Y cursor
     lcdSendCommand(SSD2119_X_RAM_ADDR_REG);
     spi16bytes(startPosX);
     lcdSendCommand(SSD2119_Y_RAM_ADDR_REG);
     spi16bytes(startPosY);
 
+    //Config the cursor to auto increment horizontally
     setCursorMode(1, 1, 0);
 
+    //Send the pixel data till the length of the line is reached
     uint16_t i;
     lcdSendCommand(SSD2119_RAM_DATA_REG);
     for(i = 0; i < length; i++)
         spi16bytes(color);
 
+    //Deselect the LCD
     selectCS(1);
 }
 
+//Draw a vertical line
 void drawLineY(uint16_t startPosY, uint16_t length, uint16_t startPosX, uint16_t color)
 {
+    //Select the LCD
     selectCS(0);
 
+    //Set the X and Y cursor
     lcdSendCommand(SSD2119_X_RAM_ADDR_REG);
     spi16bytes(startPosX);
     lcdSendCommand(SSD2119_Y_RAM_ADDR_REG);
     spi16bytes(startPosY);
 
+    //Config the cursor to auto increment vertically
     setCursorMode(1, 1, 1);
 
+    //Send the pixel data till the length of the line is reached
     uint16_t i;
     lcdSendCommand(SSD2119_RAM_DATA_REG);
     for(i = 0; i < length; i++)
         spi16bytes(color);
 
+    //Deselect the LCD
     selectCS(0);
 }
 
+//Write a string of characters on the LCD
 void writeString(uint16_t posX, uint16_t posY, uint16_t color, char *inString)
 {
-    printf("\n");
-
+    //If the string still exists, write it to the LCD
     while(*inString){
         writeLetter(posX, posY, color, *inString++);
+        //Move X to the right by the character width
         posX += 7;
     }
 }
 
-//int putstr (char *s)
-//{
-//    int i = 0;
-//    while (*s){
-//        putchar(*s++);
-//        i++;
-//    }
-//    return i+1;
-//}
-
+//Write a single letter on the LCD
 void writeLetter(uint16_t posX, uint16_t posY, uint16_t color, char inChar)
 {
+    //Select the LCD
     selectCS(0);
 
+    //Return if invalid character entered
     if(inChar < ' ' || inChar > '~')
         return;
 
+    //Offset the input if spce is detected
     if(inChar == ' ')
-            inChar = '~' + 1;
+        inChar = '~' + 1;
 
+    //Offset character to index 0
     inChar -= '!';
-//    printf("Entered Character: %d\n", inChar);
 
+    //Loop counters
     int i = 0;
     int j = 0;
+    //Start and end index of the array
     int startFont = inChar * 11;
     int endFont = (inChar * 11) + 10;
 
-//    printf("Start: %d End: %d\n", startFont, endFont);
-
+    //Cursor mode set
     setCursorMode(1, 1, 0);
     setCursorXY(posX, posY);
 
 
+    //Send command
     for(i = startFont; i <= endFont; i++){
-//        printf("IN: 0x%X ", lucidaConsole_8ptBitmaps[i]);
         lcdSendCommand(SSD2119_RAM_DATA_REG);
-
+        //Write X axis
         for(j = 7; j >= 0; j--){
+            //Write Y axis
             uint16_t temp = ((lucidaConsole_8ptBitmaps_narrow[i] >> j) & 0x01);
             if(temp){
-//                printf("1");
                 spi16bytes(color);
             } else {
-//                printf("0");
                 spi16bytes(BLACK);
             }
         }
-//        printf("\n");
 
-//        if((i + 1) % 2 == 0 && i != 0){
-            setCursorXY(posX, ++posY);
-//        }
+        //Set the new cursor position
+        setCursorXY(posX, ++posY);
 
     }
+    //Deselect the LCD
     selectCS(1);
-
 }
 
 
@@ -201,23 +243,19 @@ void EUSCIB0_IRQHandler(void)
 
     if (EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG)
     {
-        //send MSB
-        //        EUSCI_B0->TXBUF = MSB;
-        //        while (!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
-
     }
 }
 
 
-
+//Initialize the LCD
+//Complete routine referenced from
+//http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP-EXP432P401R/latest/index_FDS.html
 void lcdInit(void)
 {
     while (!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
 
+    //loop counter
     uint32_t ulCount;
-    //    volatile uint32_t i;
-
-    //    HAL_LCD_initLCD();
 
     //
     // Select the LCD for SPI comunication.
@@ -344,28 +382,9 @@ void lcdInit(void)
     for(ulCount = 0; ulCount < 76800; ulCount++)
     {
         spi16bytes(0x0000);    // Black
-        //        spi16bytes(0xF800);    // Red
-//        delay(1);
-        //        spi16bytes(0x07E0);    // Green
-        //        spi16bytes(0x001F);    // Blue
     }
 
-
-//    drawLineX(0, 5, 0, BLUE);
-//    drawLineX(10, 10, 10, RED);
-//    drawLineX(20, 15, 20, GREEN);
-//    drawLineX(30, 20, 30, BLUE);
-//
-//    drawLineY(50, 5, 0, BLUE);
-//    drawLineY(60, 10, 10, RED);
-//    drawLineY(70, 15, 20, GREEN);
-//    drawLineY(80, 20, 30, BLUE);
-
-
-
-    //
-    // Deselect the LCD for SPI comunication.
-    //
+    //deselect the LCD
     selectCS(1);
 
 }
--- lcd_driver.h
+++ lcd_driver.h
@@ -1,29 +1,48 @@
 /*
- * lcd_driver.h
+ * @file lcd_driver.h
+ * @brief The LCD driver header for the BOOSTXL-K350QVG boosterpack
  *
- *  Created on: 03-Dec-2019
- *      Author: prayag
+ * This header file contains the LCD driver function
+ * prototypes and the definitions required for the
+ * BOOSTXL-K350QVG boosterpack. It uses 4-wire SPI to
+ * communicate with the LCD, and is capable of drawing
+ * pixels, lines and text on the display
+ *
+ * @date 03-Dec-2019
+ * @author Prayag Milan Desai, Rahul Ramaprasad
  */
 
 #ifndef LCD_DRIVER_H_
 #define LCD_DRIVER_H_
 
+//The MSP432 header
 #include "msp.h"
+//Standard header files
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
+//The header file containing LCD specific macros
 #include "HAL_MSP_EXP432P401R_KITRONIX320X240_SSD2119_SPI.h"
+//The header file containing the lcd driver specific macros
 #include "kitronix320x240x16_ssd2119_spi.h"
+//the font
 #include "Lucida_Console_8pts.h"
 
-
+/* The pin numbers for the GPIOs USED TO COMMNUICATE WITH THE LCD */
+//SPI - SCLK
 #define CLK BIT5
+//SPI - SDA
 #define MOSI BIT6
+//SPI - SCS
 #define LCD_CS BIT0
+//LCD - SDC
 #define DC BIT6
+//LCD - RESET
 #define RESET BIT5
+//LCD - BACKLIGHT
 #define PWM BIT7
 
+/* The macros defining common 16-bit colours for the LCD */
 #define RED 0xF800
 #define GREEN 0x07E0
 #define BLUE 0x001F
@@ -31,20 +50,185 @@
 #define WHITE (RED | GREEN | BLUE)
 
 
-
+/*
+ * @brief Initialize the LCD
+ *
+ * This function intitializes the LCD using the
+ * routine described in the example provided
+ * by texas instruments found at
+ * http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP-EXP432P401R/latest/index_FDS.html
+ *
+ * @return void
+ */
 void lcdInit(void);
+
+
+/*
+ * @brief Send the register comnmand
+ *
+ * This function selects the SDC line, sends the
+ * register address where data is to be written,
+ * and deselects the SDC line
+ *
+ * @param The register command address to be written
+ * @return void
+ */
 void lcdSendCommand(uint8_t command);
+
+/*
+ * @brief Send 2 bytes of SPI data
+ *
+ * This function sends in total 16 bits of
+ * data via SPI, MSB first.
+ * Uses polling approach
+ *
+ * @param 16 bits of data to be sent
+ * @return void
+ */
 void spi16bytes(uint16_t in);
+
+/*
+ * @brief Send 1 byte of SPI data
+ *
+ * This function sends in total 8 bits of
+ * data via SPI, MSB first.
+ * Uses polling approach
+ *
+ * @param 8 bits of data to be sent
+ * @return void
+ */
 void spi8bytes(uint8_t in);
+
+/*
+ * @brief Selects/Deselects CS pin
+ *
+ * Toggles the SCS GPIO high or low,
+ * depending upon the parameter.
+ * Active low, selects the LCD
+ *
+ * @param state of the GPIO pin, 1 = HIGH, 0 = LOW
+ * @return void
+ */
 void selectCS(int state);
+
+/*
+ * @brief Selects/Deselects DC pin
+ *
+ * Toggles the SCD GPIO high or low,
+ * depending upon the parameter.
+ * Active low, selects whether the input
+ * is command or data
+ *
+ * @param state of the GPIO pin, 1 = HIGH, 0 = LOW
+ * @return void
+ */
 void selectDC(int state);
+
+/*
+ * @brief Blocking delay in ms
+ *
+ * Blocking delay in milliseconds, using a for
+ * loop that uses the system clock speed for
+ * exact calculations
+ *
+ * Reference: http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP-EXP432P401R/latest/index_FDS.html
+ *
+ * @param amount of delay in ms
+ * @return void
+ */
 void delay(uint16_t msec);
+
+/*
+ * @brief Sets the cursor mode
+ *
+ * Sets the cursor mode of the LCD display
+ * to auto increment/decrement in horizontal/
+ * vertical direction upon GRAM data write
+ *
+ * @param id1, id0: ID[1:0] Decides the inc/dec direction
+ * @param am: Decides the cursor movement in the vertical direction
+ * @return void
+ */
 void setCursorMode(uint8_t id1, uint8_t id0, uint8_t am);
+
+/*
+ * @brief Sets a pixel to a perticular color
+ *
+ * Sets a pixel to a perticular color, requires
+ * X-Y position and the color
+ *
+ * @param posX The X axis position of the pixel to be displayed
+ * @param posY The Y axis position of the pixel to be displayed
+ * @param color The 16-bit color of the pixel
+ * @return void
+ */
 void drawPixelXY(uint16_t posX, uint16_t posY, uint16_t color);
+
+/*
+ * @brief Draw a line in horizontal direction
+ *
+ * Draws a line in the horizontal direction, given the
+ * start X-Y position, color and length
+ *
+ * @param startPosX The X axis position of the line to be displayed from
+ * @param length the length of the line, in pixels
+ * @param startPosY The Y axis position of the line to be displayed from
+ * @param color The 16-bit color of the pixel
+ * @return void
+ */
 void drawLineX(uint16_t startPosX, uint16_t length, uint16_t startPosY, uint16_t color);
+
+/*
+ * @brief Draw a line in vertical direction
+ *
+ * Draws a line in the vertical direction, given the
+ * start X-Y position, color and length
+ *
+ * @param startPosY The Y axis position of the line to be displayed from
+ * @param length the length of the line, in pixels
+ * @param startPosX The X axis position of the line to be displayed from
+ * @param color The 16-bit color of the pixel
+ * @return void
+ */
 void drawLineY(uint16_t startPosY, uint16_t length, uint16_t startPosX, uint16_t color);
+
+/*
+ * @brief Write a character on the LCD
+ *
+ * Writes a character on the LCD, given the
+ * start X-Y position, color and the input character
+ *
+ * @param posX The X axis position of the pixel to be displayed
+ * @param posY The Y axis position of the pixel to be displayed
+ * @param color The 16-bit color of the character
+ * @param inChar The character ASCII code to be written on the LCD
+ * @return void
+ */
 void writeLetter(uint16_t posX, uint16_t posY, uint16_t color, char inChar);
+
+/*
+ * @brief Sets the X-Y position of the cursor
+ *
+ * Set the X-Y position of the cursor of the LCD
+ *
+ * @param posX The X axis position of the cursor
+ * @param posY The Y axis position of the cursor
+ * @return void
+ */
 void setCursorXY(uint16_t posX, uint16_t posY);
+
+/*
+ * @brief Write a string on the LCD
+ *
+ * Writes a string on the LCD, given the
+ * start X-Y position, color and the input character
+ *
+ * @param posX The X axis position of the pixel to be displayed
+ * @param posY The Y axis position of the pixel to be displayed
+ * @param color The 16-bit color of the characters
+ * @param *inString The pointer to the character array which is to be written on the LCD
+ * @return void
+ */
 void writeString(uint16_t posX, uint16_t posY, uint16_t color, char* inString);
 
 
